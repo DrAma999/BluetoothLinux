71ae2e3f0cc95d89696d24c9d0d9ea5a9d302259
DrAma999 <andrea.alessandro@gmail.com>
2021-10-23 15:17:10 +0200

    Modified library type

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -6,6 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "BluetoothLinux",
+            type: .static,
             targets: ["BluetoothLinux"]
         )
     ],
